fff321ad440fcac845137b631678ad081539e864
andresaputra22 <andre.saputra@orderfaz.com>
2024-09-12 15:23:34 +0700

    🎉 init project

---
+++ components/content/SectionTitle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <section class="flex flex-col gap-2">
+    <h2 class="font-bold text-2xl">
+      <slot name="title"></slot>
+    </h2>
+    <h3>
+      <slot name="description"></slot>
+    </h3>
+  </section>
+</template>
---
+++ content/index.md
@@ -0,0 +1,7 @@
+::section-title
+#title
+I Wonder How
+
+#description
+This will be rendered inside the *description* slot.
+::
